d5d220c47c9aa636ee933637c10856c02d3315e2
tjdans6342 <01tjdans@naver.com>
2021-01-26 16:59:53 +0900

    create user class

---
+++ user_class.py
@@ -0,0 +1,15 @@
+class User ():
+    def __init__(self, _id, password, name, grade, solved_number, solved_list, submit_list  ):
+        self.id = _id
+        self.password = password
+        self.name = name
+        self.grade = grade
+        self.solved_number = solved_number
+        self.solved_list = solved_list
+        self.submit_list = submit_list
+
+    
+
+
+
+
